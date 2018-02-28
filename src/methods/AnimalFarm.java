5366f0328cb9da946df5e7beeeb444a1e57eda0c
codeclub <codeclub@smms-rm23-36.smusd.local>
2018-02-28 14:56:23 -0800

    Dice Roll, IntroToRandom, RockPaperScissors, Validation

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,20 +18,50 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
+	String input = JOptionPane.showInputDialog(null, "What Animal Do You Want?");
+	
+	if(input.equals("Cow"))
+		playMoo();
+	
+	if(input.equals("Cat"))
+		playCat();
+	
+	if(input.equals("Duck"))
+		playQuack();
+	
+	if(input.equals("Dog"))
+		playWoof();
+	
+	if(input.equals("Llama"))
+		playLlama();
 
-	/* 2. Make it so that the user can keep entering new animals. */
+;	/* 2. Make it so that the user can keep entering new animals. */
+	
 }
 
 void playMoo() {
 	playNoise(mooFile);
+//Moo
 }
 
 void playQuack() {
 	playNoise(quackFile);
+//Quack
 }
 
 void playWoof() {
 	playNoise(woofFile);
+//Woof
+}
+
+void playCat() {
+	playNoise(meowFile);
+//Meow
+}
+
+void playLlama() {
+	playNoise(llamaFile);
+//BAAA BAAAHHHHH
 }
 
 String quackFile = "quack.wav";
